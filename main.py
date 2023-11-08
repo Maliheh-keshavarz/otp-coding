c9ce36e750aa7d02aa0d74d03bb906cb9935588f
Maliheh-keshavarz <146880513+Maliheh-keshavarz@users.noreply.github.com>
2023-11-07 21:28:28 -0500

    step 1:part of main body

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 
+from otp_generator import otp
+
+
+
+def main():
+
+    alphabet_to_number = {chr(97 + i): i for i in range(26)}
+    number_to_alphabet = {i: chr(97 + i) for i in range(26)}
+    # number_to_alphabet = {val: k for k, val in alphabet_to_number.items()}  
+
+
+    message= input("Enter the plain text: ")
+    plain_text =message.lower()
+    plain_text=plain_text.replace(" ", "")
+    
+    
 
 
 
 
 
+if __name__ == "__main__":
+    main()
---
+++ otp_generator.py
@@ -0,0 +1,4 @@
+
+
+def otp(param):
+    pass
